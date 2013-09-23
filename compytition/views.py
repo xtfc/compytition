0be59ed86e0fff032522e519bb74ced50653841b
John Weachock <jweachock@gmail.com>
2013-09-23 17:24:06 -0400

    Add simple scoreboard template

--- compytition/views.py
+++ compytition/views.py
@@ -3,4 +3,8 @@ from compytition import app
 
 @app.route('/')
 def index():
-	return flask.render_template('index.html', content='<p>Hello, world!</p>')
+	scoreboard = []
+	scoreboard.append({'name': 'John', 'score': 10})
+	scoreboard.append({'name': 'Phil', 'score': 0})
+	scoreboard.append({'name': 'Tyler', 'score': -10})
+	return flask.render_template('scoreboard.html', scoreboard=scoreboard)
