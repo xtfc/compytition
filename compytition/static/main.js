2b8808bc716c98bbff40a7c045144afe1d69b8f4
John Weachock <jweachock@gmail.com>
2013-09-24 12:18:17 -0400

    Fix question tabs

--- compytition/static/main.js
+++ compytition/static/main.js
@@ -1,15 +1,17 @@
-$(window).bind('hashchange', function() {
+function update_question(event) {
+	if(event) event.preventDefault();
+	if(this.hash) window.location.hash = this.hash;
+
 	$('#questions .question').hide();
 	$('#questions .question-button').removeClass('pure-menu-selected');
 	$(window.location.hash).show();
-	$(window.location.hash + 'b').addClass('pure-menu-selected');
-});
+	$(window.location.hash + '-button').addClass('pure-menu-selected');
+}
 
 $(function() {
-	var id = '#q1';
-	if(window.location.hash) {
-		id = window.location.hash;
+	if(!window.location.hash) {
+		window.location.hash = '#q1';
 	}
-	$(id).show();
-	$(id + 'b').addClass('pure-menu-selected');
+	update_question();
+	$('#questions .question-button a').click(update_question);
 });
