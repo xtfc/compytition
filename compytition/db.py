98856f094fb63e36daae3b735ba650f87114ba13
John Weachock <jweachock@gmail.com>
2013-09-25 12:29:41 -0400

    Fix bugs in db module

--- compytition/db.py
+++ compytition/db.py
@@ -4,7 +4,7 @@ from contextlib import closing
 from compytition import app
 
 def init():
-	with closing(connect()) as db:
+	with closing(sqlite3.connect(app.config['DATABASE'])) as db:
 		with app.open_resource('schema.sql', mode='r') as f:
 			db.cursor().executescript(f.read())
 		db.commit()
@@ -24,8 +24,8 @@ def query(query, args=(), one=False):
 	return (rv[0] if rv else None) if one else rv
 
 def execute(query, args=()):
-	g.db.execute(query, args)
+	flask.g.db.execute(query, args)
 
 def commit():
-	g.db.commit()
+	flask.g.db.commit()
 
