23835f310f7b3740d1d561c37ef4149e2632f82e
John Weachock <jweachock@gmail.com>
2013-09-29 19:48:53 -0400

    Add a seconds->time filter

--- compytition/filters.py
+++ compytition/filters.py
@@ -1,3 +1,8 @@
+import time
 from compytition import app
 
-# NYI
+@app.template_filter('time')
+def filter_time(seconds):
+	if int(seconds) == 0:
+		return '&#8210;'
+	return time.strftime("%H:%M:%S", time.gmtime(float(seconds)))
